3414a1e13afb5496d26b3a1eab4693c3a9bdba88
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2021-12-22 16:33:39 +0000

    tasks complete

--- main.js
+++ main.js
@@ -33,36 +33,6 @@ function would return "This is an alien language"
 
 //👉 Write the function your CodeWarriors will start with below here:
 export function scoreWord (word1, word2) {
-    if(word1.match(/[A-Za-z]+/) && word2.match(/[A-Za-z]+/)) {
-    if(word1.length === word2.length) {
-let word1Score = 0;
-let word2Score = 0;
-let vowels = ['a', 'e', 'i', 'o', 'u', "A", "E", "I", "O", "U"]
-let result = [];
-
-for (let i=0; i<word1.length; i++){
-    if(vowels.includes(word1[i])){word1Score += 5} else {word1Score += 2}
-}
-
-for (let i=0; i<word2.length; i++){
-    if(vowels.includes(word2[i])){word2Score += 5} else {word2Score += 2}
-}
-
-if(word1Score > word2Score){
-    result.push(word1)
-    result.push(word1Score)
-} else if (word2Score > word1Score) {
-    result.push(word2)
-    result.push(word2Score)
-} else {
-    result.push("tie")
-    result.push(word1Score)
-}
-    return result;
-    } 
-    else {
-        return "It's not a fair fight!"
-    }} else {
-        return "This is an alien language"
-    }
+    //Write your function here
+    return 
 }
