2313b5bf37cbcd86fe86eae8110bfa9ec12ad735
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2021-12-22 16:31:25 +0000

    Step 1 and 2 complete

--- main.js
+++ main.js
@@ -2,32 +2,42 @@
 👉 Write your kata here!
 
 Your function will take in two words. 
+
+If a word includes any characters that aren't letters, return "This is an alien language"
+The words must be the same number of letters. If the words do not have the same number of letters, return "It's not a fair fight!"
+
 Each letter in each word has a points value. 
 Vowels will score 5 points each, consonants will score 2 points each.
 Your function must return the word which has the highest score, along with the score, in the form: ["word", score]
 If the score is a draw, return ["tie", score]
 
-Inputs will always be strings
-
 Example:
 
-function("hello", "goodbye"):
-function would return ["goodbye", 23]
+function("hello", "alien"):
+function would return ["alien", 19]
 
-function("HOSPITAL", "HOTEL"):
-function would return ["HOSPITAL", 25]
+function("HOTEL", "CRAMP"):
+function would return ["HOTEL", 16]
 
 function("HELLO", "apple"):
 function would return ["tie", 16]
+
+function("dopamine", "apple"):
+function would return "It's not a fair fight!"
+
+function("letters", "%--"):
+function would return "This is an alien language"
 
 */
 
 
 //👉 Write the function your CodeWarriors will start with below here:
 export function scoreWord (word1, word2) {
+    if(word1.match(/[A-Za-z]+/) && word2.match(/[A-Za-z]+/)) {
+    if(word1.length === word2.length) {
 let word1Score = 0;
 let word2Score = 0;
-let vowels = ['a', 'e', 'i', 'o', 'u']
+let vowels = ['a', 'e', 'i', 'o', 'u', "A", "E", "I", "O", "U"]
 let result = [];
 
 for (let i=0; i<word1.length; i++){
@@ -49,4 +59,10 @@ if(word1Score > word2Score){
     result.push(word1Score)
 }
     return result;
+    } 
+    else {
+        return "It's not a fair fight!"
+    }} else {
+        return "This is an alien language"
     }
+}
--- main.test.js
+++ main.test.js
@@ -26,29 +26,69 @@ function would return ["tie", 16]
 */
 
 
+import { createTestScheduler } from '@jest/core';
 import { scoreWord } from './main.js';
 
 // check the format of response is correct - i.e. array with 2 elements
 // check array[0] = string, array[1] = number
 
-test("Response should be an array with two elements, the first a string, the second a number", function(){
+//Check format of array
+
+test("For words of the same length, the response should be an array with two elements, the first a string, the second a number", function(){
 const actual = scoreWord("wordOne", "wordTwo")
 const expected = [expect.any(String), expect.any(Number)]
 expect(actual).toStrictEqual(expected);
 })
 
-test("For inputs of 'hello' and 'goodbye', should return ['goodbye', 23]", function(){
-    const actual = scoreWord("hello", "goodbye")
-    const expected = ["goodbye", 23]
+//Checks words of different lengths
+
+test("For words of different lengths, the response should be the phrase 'It's not a fair fight!'", function() {
+    const actual = scoreWord("wordOne", "word")
+    const expected = "It's not a fair fight!"
+    expect(actual).toBe(expected)
+})
+
+//Checks words only have letter characters
+
+test("For inputs that includes non-letter characters, the response should the phrase 'This is an alien language'", function() {
+    const actual = scoreWord("192-", "%$%h")
+    const expected = "This is an alien language"
+    expect(actual).toStrictEqual(expected)
+})
+
+//check it works for variety of word inputs
+
+test("For inputs of 'hello' and 'posts', should return ['hello', 16]", function(){
+    const actual = scoreWord("hello", "posts")
+    const expected = ["hello", 16]
     expect(actual).toStrictEqual(expected);
     })
 
-test("For inputs of 'hospital' and 'hotel', should return ['hospital', 25]", function(){
-    const actual = scoreWord("hospital", "hotel")
-    const expected = ["hospital", 25]
+test("For inputs of 'hospital' and 'dopamine', should return ['dopamine', 28]", function(){
+    const actual = scoreWord("hospital", "dopamine")
+    const expected = ["dopamine", 28]
     expect(actual).toStrictEqual(expected);
      })
+  
+//check it works for ties 
+
+test("For inputs of 'apple' and 'toast', should return ['tie', 16]", function(){
+    const actual = scoreWord("apple", "toast")
+    const expected = ["tie", 16]
+    expect(actual).toStrictEqual(expected);
+         })
 
 
-//check it works for variety of word inputs
 //check it works for upper and lower case
+
+test("For inputs of 'APPLE' and 'toast', should return ['tie', 16]", function(){
+    const actual = scoreWord("APPLE", "toast")
+    const expected = ["tie", 16]
+    expect(actual).toStrictEqual(expected);
+         })
+
+test("For inputs of 'ALIENS' and 'SECRET', should return ['ALIENS', 21]", function(){
+    const actual = scoreWord("ALIENS", "SECRET")
+    const expected = ["ALIENS", 21]
+    expect(actual).toStrictEqual(expected);
+                 })
